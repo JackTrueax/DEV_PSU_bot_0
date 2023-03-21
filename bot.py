e3cfb44fc34400eaf56e65edcfcb4f63a8cd6eeb
Jack Trueax <jack.r.trueax@gmail.com>
2023-03-21 19:09:55 -0400

    modified ttt message format and import statement

--- bot.py
+++ bot.py
@@ -3,41 +3,8 @@ import os
 # import random
 from dotenv import load_dotenv
 from typing import Union
-
-# import tictiactoe
-class ttt():
-    # inp = input("7 | 8 | 9\n--+---+--\n4 | 5 | 6\n--+---+--\n1 | 2 | 3 \n")
-    board = []
-    b = {1:6, 2:7, 3:8, 4:3, 5:4, 6:5, 7:0, 8:1, 9:2}
-    def __init__(self):
-        # self.board = ['_' for x in range(9)]
-        self.board = [' ' for x in range(9)]
-
-    def check_win(self, mark):
-        if {self.board[0], self.board[1], self.board[2]} == {mark} or {self.board[3], self.board[4], self.board[5]} == {mark} or {self.board[6], self.board[7], self.board[8]} == {mark} or {self.board[0], self.board[3], self.board[6]} == {mark} or {self.board[1], self.board[4], self.board[7]} == {mark} or {self.board[2], self.board[5], self.board[8]} == {mark} or {self.board[0], self.board[4], self.board[8]} == {mark} or {self.board[2], self.board[4], self.board[6]} == {mark}:
-            return 1
-        elif " " not in self.board:
-            return 2
-        else:
-            return 0
-
-    def update_board(self, inp, mark):
-        print(mark)
-        try:
-            pos = self.b[int(inp)]
-        except:
-            print("error parsing input")
-            return -1
-        if self.board[pos] != " ": return -1
-        # if self.board[pos] != "_": return -1
-        print
-        if mark == "ttt1":
-            mark = "X"
-        if mark == "ttt2":
-            mark = "O"
-        self.board[pos] = mark
-        return self.check_win(mark)
-        
+from discord import Message
+from ttt import ttt
 
 load_dotenv()
 token = os.getenv('TOKEN')
@@ -47,8 +14,12 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user}!')
         self.state = "message"
 
+    async def on_typing(self, channel, user, when):
+        print(f"{user} is typing message in {channel} {when}")
+
     async def on_message(self, message):
         print(f'Message from {message.author} in {message.channel}: {message.content}')
+        print(message.attachments)
 
         if message.author == client.user:
             print("ignoring bot message")
@@ -65,6 +36,10 @@ class MyClient(discord.Client):
             print(f"\tsending message: hello")
             await message.channel.send("hello")
 
+        if "react" in message.content:
+            print(f"\treacting with 👍")
+            await message.add_reaction("👍")
+            await message.add_reaction("👽")
         
         if message.content == "ttt":
             self.state = "ttti"
@@ -75,7 +50,7 @@ class MyClient(discord.Client):
             self.state = "ttt1"
             self.player_2 = message.mentions[0]
             await message.channel.send(f"player 2 is {self.player_2}")
-            await message.channel.send("use the numberpad for input:\n7 | 8 | 9\n--+---+--\n4 | 5 | 6\n--+---+--\n1 | 2 | 3 \n")
+            await message.channel.send("use the numberpad for input:\n```7 | 8 | 9\n--+---+--\n4 | 5 | 6\n--+---+--\n1 | 2 | 3 \n```")
             await message.channel.send(f"{self.player_1}'s turn")
             self.game = ttt()
 
@@ -100,7 +75,7 @@ class MyClient(discord.Client):
                 self.state = "message"
 
 def format_board(x):
-    return f"{x[0]} | {x[1]} | {x[2]}\n--+---+--\n{x[3]} | {x[4]} | {x[5]}\n--+---+--\n{x[6]} | {x[7]} | {x[8]} \n"
+    return f"```{x[0]} | {x[1]} | {x[2]}\n--+---+--\n{x[3]} | {x[4]} | {x[5]}\n--+---+--\n{x[6]} | {x[7]} | {x[8]} \n```"
 
 
 # intents = discord.Intents.default()
